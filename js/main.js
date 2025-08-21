dc77ae7f084034d04aa6c235eb2e7ac1d419d4dc
Chtholly-rf <lorenbaigo1@gmail.com>
2025-08-20 22:33:55 -0300

    Este si.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Unas flores con mucho cariño').split('');
+    const titles = ('sos un sol que lo ilumina todo').split('');
 const titleElement = document.getElementById('title');
 let index = 0;
 
@@ -17,7 +17,7 @@ function appendTitle() {
 
 // Crear un nuevo elemento de texto adicional
 const additionalText = document.createElement("span");
-additionalText.innerHTML = "sos un sol que lo ilumina todo";
+additionalText.innerHTML = "todo mi cariño esta en estas bellas flores";
 additionalText.style.position = "absolute";
 additionalText.style.top = "0";
 additionalText.style.left = "100%";  // Esto lo coloca a la derecha
